2af7a1cb5396f756b12f2a9f3054bea1eb46ca48
juangonzalezmo <juagonzalezmo@unal.edu.co>
2024-02-23 23:00:09 -0500

    Version 1
    Se crean todas las clases con sus atributos y métodos, además los atributos se encapsulan lo que mas se puede.

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,73 @@
+package taller3.televisores;
+
+public class Control {
+	
+	private TV tv;
+	
+	public void enlazar(TV tv) {
+		
+		this.tv=tv;
+		tv.control=this;
+	}
+		
+	public void turnOn() {
+		
+		this.tv.estado = true;
+	}
+	
+	public void turnOff() {
+		
+		this.tv.estado = false;
+	}
+	
+	public void canalUp() {
+		
+		if (this.tv.canal>=1 && this.tv.canal<120 && this.tv.estado==true ) {
+			
+			this.tv.canal++;
+		}
+	}
+	
+	public void canalDown() {
+		
+		if (this.tv.canal>1 && this.tv.canal<=120 && this.tv.estado==true ) {
+			
+			this.tv.canal--;
+		}
+	}
+	
+	public void volumenUp() {
+		
+		if (this.tv.volumen>=0 && this.tv.volumen<7 && this.tv.estado==true ) {
+			
+			this.tv.volumen++;
+		}
+	}
+	
+	public void volumenDown() {
+		
+		if (this.tv.volumen>0 && this.tv.volumen<=7 && this.tv.estado==true ) {
+			
+			this.tv.volumen--;
+		}
+	}
+	
+	public void setCanal(int canal) {
+		this.tv.canal = canal;
+	}
+	
+	public void setVolumen(int volumen) {
+		this.tv.volumen = volumen;
+	}
+
+	public TV getTv() {
+		return tv;
+	}
+
+	public void setTv(TV tv) {
+		this.tv = tv;
+	}
+	
+	
+
+}
